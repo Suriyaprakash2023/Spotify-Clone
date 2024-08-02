c90d8b8fe210bb53b85e421af9b7c09efb2e3197
Suriyaprakash2023 <150240754+Suriyaprakash2023@users.noreply.github.com>
2024-08-02 06:37:32 +0530

    login page setup

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='login'>
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+       <button>LOGIN TO SPOTIFY</button>
+    </div>
+  )
+}
+
+export default Login
